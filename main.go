afd526355427b2af580c050d196fecad7e680895
Dima Koss <koss@null.net>
2023-04-08 00:58:39 +0300

    changed FStorage

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"os"
+
+	"github.com/koss-null/SlothLS/ostools"
+)
+
 func main() {
+	fileTreeStorage := ostools.NewFStorage("")
+	calledPath, err := os.Executable()
+	if err != nil {
+		panic("Failed to get the path binary was called from")
+	}
+	fileTreeStorage.ReadPath(calledPath)
 
 }
--- ostools/filesystem/storage.go
+++ ostools/filesystem/storage.go
@@ -2,20 +2,39 @@ package filesystem
 
 import "sync"
 
-type globalFSStorage struct {
+type FStorage struct {
 	path2Object map[string]FSObject
+	mx          *sync.Mutex
 }
 
-var (
-	gfs globalFSStorage
-	mx  sync.Mutex
-)
+func NewFStorage(cachePaths ...string) FStorage {
+	if len(cachePaths) > 0 {
+		totalCachedData := make(map[string]FSObject)
+		for _, path := range cachePaths {
+			cachedData, err := readCache(path)
+			if err != nil {
+				// TODO write the error into a log file
+				cachedData = make(map[string]FSObject)
+			}
+			totalCachedData = joinMap(totalCachedData, cachedData)
+		}
 
-func ReadPath(path string) (FSObject, error) {
-	mx.Lock()
-	defer mx.Unlock()
+		return FStorage{
+			totalCachedData,
+			&sync.Mutex{},
+		}
+	}
+	return FStorage{
+		make(map[string]FSObject),
+		&sync.Mutex{},
+	}
+}
 
-	obj, found := gfs.path2Object[path]
+func (s FStorage) ReadPath(path string) (FSObject, error) {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+
+	obj, found := s.path2Object[path]
 	if found && !needUpgrade(obj) {
 		return obj, nil
 	}
@@ -24,14 +43,34 @@ func ReadPath(path string) (FSObject, error) {
 	if err != nil {
 		return NilFile(), err
 	}
-	gfs.path2Object[path] = obj
+	s.path2Object[path] = obj
+	return obj, nil
 }
 
 func readPath(path string) (FSObject, error) {
 
 }
 
+func readCache(path string) (map[string]FSObject, error) {
+	// TODO: implement
+	return make(map[string]FSObject), nil
+}
+
 func needUpgrade(obj FSObject) bool {
 	// TODO: impl
 	return false
+}
+
+func joinMap(mx map[string]FSObject, my map[string]FSObject) map[string]FSObject {
+	if len(mx) < len(my) {
+		mx, my = my, mx
+	}
+
+	for k, v := range my {
+		if _, ok := mx[k]; !ok {
+			mx[k] = v
+		}
+	}
+
+	return mx
 }
