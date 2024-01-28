725edf473b6a3a831d46f5f6eea01618885ae2ae
Dima Koss <koss@null.net>
2024-01-29 02:31:02 +0300

    add icons for some file types

--- simple/print_folder_content.go
+++ simple/print_folder_content.go
@@ -3,16 +3,12 @@ package simple
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/koss-null/funcfrog/pkg/ff"
+	"github.com/koss-null/funcfrog/pkg/pipe"
 	"github.com/koss-null/funcfrog/pkg/pipies"
 	"github.com/pkg/errors"
-)
-
-const (
-	BlueColor  = "\033[34m"
-	GreenColor = "\033[32m"
-	ResetColor = "\033[0m"
 )
 
 func ListDir(dir string) error {
@@ -27,16 +23,27 @@ func ListDir(dir string) error {
 		return errors.WithStack(err)
 	}
 
-	dirs := ff.MapFilter(files, func(file os.FileInfo) (string, bool) {
-		return GreenColor + file.Name() + "/" + ResetColor, file.IsDir()
-	}).Sort(pipies.Less[string])
-	fls := ff.MapFilter(files, func(file os.FileInfo) (string, bool) {
-		return BlueColor + file.Name() + ResetColor, !file.IsDir()
-	}).Sort(pipies.Less[string])
+	dirs := pipe.Map(
+		ff.Filter(files, func(file *os.FileInfo) bool { return (*file).IsDir() }),
+		os.FileInfo.Name,
+	).
+		Map(addFolderIcon).
+		Map(addFolderColor).
+		Sort(pipies.Less[string])
 
-	addNewLine := func(s string) string { return s + "\n" }
-	fmt.Printf(*(fls.Map(addNewLine).Reduce(pipies.Sum[string])))
-	fmt.Printf(*(dirs.Map(addNewLine).Reduce(pipies.Sum[string])))
+	fls := pipe.Map(
+		ff.Filter(files, func(file *os.FileInfo) bool { return !(*file).IsDir() }),
+		os.FileInfo.Name,
+	).
+		Map(addFileIcon).
+		Map(addFileColor).
+		Sort(pipies.Less[string])
+
+	addTabs := func(s string) string { return s + "\t" }
+	fmt.Printf(*(fls.Map(addTabs).Reduce(pipies.Sum[string])))
+	fmt.Println()
+	fmt.Printf(*(dirs.Map(addTabs).Reduce(pipies.Sum[string])))
+	fmt.Println()
 
 	return nil
 }
@@ -45,4 +52,60 @@ func ListCurDir() error {
 	const currentDir = "."
 
 	return ListDir(currentDir)
+}
+
+func addFolderIcon(s string) string {
+	const icon = "📁"
+	const separator = ""
+	return icon + separator + s
+}
+
+func addFolderColor(s string) string {
+	const greenColor = "\033[32m"
+	return addColor(s, greenColor)
+}
+
+func addFileIcon(s string) string {
+	const separator = ""
+	const extensionSeparator = "."
+	splittedFileName := strings.Split(s, extensionSeparator)
+	return iconByExtension(splittedFileName[len(splittedFileName)-1]) + separator + s
+}
+
+func iconByExtension(ext string) string {
+	const (
+		fileIcon   = "📄"
+		goIcon     = "🐹"
+		pythonIcon = "🐍"
+		cIcon      = "<C>"
+		jsIcon     = "<JS>"
+		shIcon     = "🐚"
+		configIcon = `⚙️ `
+	)
+	switch ext {
+	case "go", "mod":
+		return goIcon
+	case "py", "wheel", "__init__":
+		return pythonIcon
+	case "c", "cpp", "h":
+		return cIcon
+	case "js":
+		return jsIcon
+	case "cfg", "yaml", "toml", "conf", "json":
+		return configIcon
+	case "sh", "bash":
+		return shIcon
+	default:
+		return fileIcon
+	}
+}
+
+func addFileColor(s string) string {
+	const blueColor = "\033[34m"
+	return addColor(s, blueColor)
+}
+
+func addColor(s, color string) string {
+	const ResetColor = "\033[0m"
+	return color + s + ResetColor
 }
